2b580894d2bf10fa6cfe9baf4ac452d9f19e75be
root <root@glint.funzt-halt.net>
2010-05-31 13:55:41 +0200

    fix add_edge invocation

--- pyweb/openvpn/views.py
+++ pyweb/openvpn/views.py
@@ -4,7 +4,7 @@ import tempfile
 import gv
 
 from pygraph.readwrite.dot		import write
-from pygraph.classes.Digraph		import digraph
+from pygraph.classes.digraph		import digraph
 from pygraph.algorithms.searching	import breadth_first_search
 
 from django.http			import HttpResponse
@@ -31,13 +31,13 @@ def nodegraph( request ):
 				( "color", color ),
 				( "weight", "10" ),
 				) )
-			
-			gr.add_edge( curnode.name, client.username, attrs=(
-				( "dir",    "back" ),
-				) )
+			gr.add_edge( (curnode.name, client.username) )
+			#gr.add_edge( curnode.name, client.username, attrs=(
+			#	( "dir",    "back" ),
+			#	) )
 	
 	for curlink in NodeLink.objects.all():
-		gr.add_edge( curlink.left.name, curlink.right.name, attrs=(
+		gr.add_edge( ( curlink.left.name, curlink.right.name ), attrs=(
 			( "taillabel", ".%s" %  curlink.left_address.split(".")[3] ),
 			( "headlabel", ".%s" % curlink.right_address.split(".")[3] ),
 			( "dir", "none" ),
@@ -49,10 +49,7 @@ def nodegraph( request ):
 	
 	# Yeah. I know. Tell me how libgv accepts file objects please.
 	tfile = tempfile.NamedTemporaryFile()
-	
 	gv.render( gvv, 'png', tfile.name )
-	
-	tfile.seek(0)
 	response = HttpResponse( mimetype='image/png', content=tfile.read() )
 	tfile.close()
 	
